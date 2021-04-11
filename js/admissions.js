42d8f608e8cc4db2b953158bb9328fe7678ab6aa
Kwesi Asamoah Jnr <b.asamoah.jnr@yasobafinibus.com>
2021-04-11 14:58:26 +0000

    admissions

---
+++ js/admissions.js
@@ -0,0 +1,42 @@
+$(function () {
+
+    window.verifyRecaptchaCallback = function (response) {
+        $('input[data-recaptcha]').val(response).trigger('change');
+    }
+
+    window.expiredRecaptchaCallback = function () {
+        $('input[data-recaptcha]').val("").trigger('change');
+    }
+
+    var contactForm = '#contact-form';
+
+    $(contactForm).validator();
+
+    $(contactForm).on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data) {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.program;
+
+                    var alertBox = '<div class="alert '
+                        + messageAlert
+                        + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'
+                        + messageText + '</div>';
+
+                    if (messageAlert && messageText) {
+                        $(contactForm).find('.messages').html(alertBox);
+                        $(contactForm)[0].reset();
+                        grecaptcha.reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
+});
